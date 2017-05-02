d0d74b852e7671e877c7a2d493d4c4850d1d56f2
Alfa Dev <rafael.mediario@gmail.com>
2017-05-01 23:11:31 -0300

    Group diaper category

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
 	def show
-		@user_products = current_user.products
+		@user_products 		  = current_user.products.where.not(category: "diaper")
+		@user_products_diaper = current_user.products.where(category: "diaper")
 	end
 
 end
