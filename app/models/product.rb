48f86d0d3ab8e2a3aa1bb5680ce2fa291cc92040
Alfa Dev <rafael.mediario@gmail.com>
2017-05-17 22:46:36 -0300

    Order diapers

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,28 @@ class Product < ApplicationRecord
 
 	validates :name, presence: true
 
-	def self.default_scope
-    	where(enabled: true)
-  	end
+  DIAPER_ORDERED = ['Tamanho RN', 'Tamanho P', 'Tamanho M', 'Tamanho G']
 
-  	scope :include_disabled, -> { unscope(:where) }
-  	scope :diaper, -> { where(category: 'diaper') }
-  	scope :low,    -> { where(category: 'low') }
-  	scope :med,    -> { where(category: 'med') }
-  	scope :high,   -> { where(category: 'high') }
+	def self.default_scope
+    where(enabled: true)
+  end
+
+	scope :include_disabled, -> { unscope(:where) }
+	scope :diaper, -> { where(category: 'diaper').by_name }
+  scope :low,    -> { where(category: 'low') }
+  scope :med,    -> { where(category: 'med') }
+  scope :high,   -> { where(category: 'high') }
+
+  scope :by_name, -> { order(order_by_case) }
+
+  private
+
+  def self.order_by_case
+    ret = "CASE"
+    DIAPER_ORDERED.each_with_index do |p, i|
+      ret << " WHEN name = '#{p}' THEN #{i}"
+    end
+    ret << " END"
+  end
+
 end
